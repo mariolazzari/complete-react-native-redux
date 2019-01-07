b94d4518b610cecd8e906a52d168c3fea18269d6
mariolazzari <mario.lazzari@gmail.com>
2019-01-07 21:36:05 +0100

    Done lesson 5 chapter 8

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,18 +1,42 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Image } from "react-native";
 import Card from "./Card";
 import CardSection from "./CardSection";
 
 const AlbumDetail = props => {
-  const { title } = props.album;
+  console.log(props.album);
+
+  const { title, artist, thumbnail_image } = props.album;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+  } = styles;
 
   return (
     <Card>
       <CardSection>
-        <Text> {title} </Text>
+        <View>
+          <Image source={{ uri: thumbnail_image }} style={thumbnailStyle} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text> {title} </Text>
+          <Text> {artist} </Text>
+        </View>
       </CardSection>
     </Card>
   );
 };
 
+const styles = {
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around"
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {}
+};
 export default AlbumDetail;
